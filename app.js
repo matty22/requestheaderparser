1081b02dfb389c0cbfa03e4a799a0203cbb00c76
Matt <matt.leonard.22@gmail.com>
2016-12-05 15:22:52 -0800

    c

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
+app.get('/', function(request, response) {
+  response.send(request.header);
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
